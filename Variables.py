52523a1ca0decd1da1b39ff8d9b4e354b69a2de2
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-09-01 16:33:04 +0530

    just made

--- Variables.py
+++ Variables.py
@@ -1,9 +1,34 @@
 # a variable is caontainer in which we can store value
 # a variable acts as per the value it is assigned
 
+#strings
 first_name = "Himanshu"
 food = "wada pav"
+email = "himanshu@fake.com"
 
 print(first_name)
 print(f"Hello {first_name}")
 print(f"you like {food}")
+print(f"your email is {email}")
+
+#integers
+age = 18
+quantity = 4
+
+print(f"Your age is {age}")
+print(f"you have purchased {quantity} items")
+
+#float
+prize = 39.98
+cgpa = 98.78
+
+print(f"your total is {prize}")
+print(f"your cgpa is {cgpa}")
+
+#boolean
+is_student = False
+
+if is_student:
+    print("you are an student")
+else:
+    print("you are NOT a student")
