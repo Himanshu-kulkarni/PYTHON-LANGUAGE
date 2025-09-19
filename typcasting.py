16c5ac8d7f2627379b80df7f04f86be66fd6f793
Himanshu-kulkarni <kulhimanshu2407@gmail.com>
2025-09-19 19:09:04 +0530

    just learned

---
+++ typcasting.py
@@ -0,0 +1,13 @@
+#typecasting is a process of converting one variable from one data type to another data type
+
+name = "Himanshu kulkarni"
+age = 18
+cgpa = 9.8
+is_student = True
+
+age = str(age)
+
+age += "1"
+
+print(type(age))
+print(age)
---
+++ typecasting01.py
@@ -0,0 +1,11 @@
+# type casting= with the help of type casting we can convert one variable into another variable
+
+name = "himansu"
+age = 25
+cgpa = 8.5
+is_student= True
+
+name = bool(name)
+
+print(type(name))
+print(name)
